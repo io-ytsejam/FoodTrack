7c557cc6734b06d0a8f2bcbadf7dc5379bbca9fc
Czarek Wójcik <czarek.wojcik98@gmail.com>
2020-03-04 19:49:46 +0100

    Better comment entity implementation

---
+++ backend/Models/Comment.java
@@ -0,0 +1,40 @@
+package Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Comment
+{
+    @Id @GeneratedValue
+    @Column(name="COMMENT_ID", nullable=false)
+    private int commentId;
+
+    @Column(name="CONTENT", nullable=false)
+    private String content;
+
+    public Comment(int commentId, String content) {
+        this.commentId = commentId;
+        this.content=content;
+    }
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
+
