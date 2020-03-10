c97e9175a3ca3bde1d92e2bca6b641350eb03694
czarus98 <czarek.wojcik98@gmail.com>
2020-03-10 17:41:12 +0100

    Added main class for testing

--- backend/main/java/com/backend/BackendApplication.java
+++ backend/main/java/com/backend/BackendApplication.java
@@ -1,13 +1,27 @@
-package backend.main.java.com.backend;
+package main.java.com.backend;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import main.java.Models.*;
 
-@SpringBootApplication
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		//SpringApplication.run(BackendApplication.class, args);
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JavaHelps"); //name of persistence unit here
+		EntityManager entityManager = factory.createEntityManager();
+
+
+		entityManager.getTransaction().begin();
+
+		// Here you can put database changes (Insert etc.)
+
+		entityManager.getTransaction().commit();
+
+		entityManager.close();
+		factory.close();
 	}
 
 }
