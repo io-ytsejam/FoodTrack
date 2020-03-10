937051c57e95745d4bc5766a2b0061e9b0fde84c
czarus98 <czarek.wojcik98@gmail.com>
2020-03-10 23:45:51 +0100

    Added inheritance and SpringBoot addnotation

--- src/main/java/com/backend/BackendApplication.java
+++ src/main/java/com/backend/BackendApplication.java
@@ -1,24 +1,32 @@
-package main.java.com.backend;
+package com.backend;
 
-import main.java.Models.*;
+import Models.PersonEntity;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class BackendApplication {
+@SpringBootApplication
+public class BackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JavaHelps"); //name of persistence unit here
 		EntityManager entityManager = factory.createEntityManager();
-
+		PersonEntity person=new PersonEntity();
+		person.setFirstname("Czarek");
+		person.setLastname("Alalala");
+		person.setNickname("czarus87");
+		person.setPassword("test");
 
 		entityManager.getTransaction().begin();
 
+		entityManager.persist(person);
 		// Here you can put database changes (Insert etc.)
-
 		entityManager.getTransaction().commit();
 
 		entityManager.close();
