849adcb919e29a3882e282a62ec4d6fe26efd405
czarus98 <czarek.wojcik98@gmail.com>
2020-04-23 12:14:35 +0200

    Prepared jwt token generation after logging

--- src/main/java/com/backend/Controllers/PersonLoginController.java
+++ src/main/java/com/backend/Controllers/PersonLoginController.java
@@ -1,17 +1,53 @@
 package com.backend.Controllers;
 
+import com.backend.Models.AuthToken;
+import com.backend.Models.LoginUser;
+import com.backend.Models.PersonEntity;
+import com.backend.Repositories.PersonEntityRepository;
+import com.backend.Security.JwtTokenUtil;
+import com.backend.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.naming.AuthenticationException;
 
 @Controller
-@RequestMapping("/login")
 public class PersonLoginController {
 
-    @GetMapping()
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
     public String login(Model model) {
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity register(LoginUser loginUser) throws AuthenticationException {
+
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginUser.getUsername(),
+                        loginUser.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final PersonEntity user = userService.findByNickname(loginUser.getUsername());
+        final String token = jwtTokenUtil.generateToken(user);
+        return ResponseEntity.ok(new AuthToken(token,loginUser.getUsername()));
     }
 }
