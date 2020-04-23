42f2b9646197283a63a6328a9492fcc92f2846c9
czarus98 <czarek.wojcik98@gmail.com>
2020-04-23 16:03:37 +0200

    Prepared jwt token generation after logging

--- src/main/java/com/backend/Controllers/PersonLoginController.java
+++ src/main/java/com/backend/Controllers/PersonLoginController.java
@@ -37,7 +37,7 @@ public class PersonLoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity register(LoginUser loginUser) throws AuthenticationException {
+    public ResponseEntity register(@RequestBody LoginUser loginUser) throws AuthenticationException {
 
         final Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
