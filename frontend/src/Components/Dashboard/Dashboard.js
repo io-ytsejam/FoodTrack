97be045fdd5ec930e2440342a5d616471933ebfd
Marcin Skrzymowski <44101560+io-ytsejam@users.noreply.github.com>
2020-05-23 00:49:44 +0200

    Update Dashboard.js

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -20,7 +20,7 @@ class Dashboard extends Component {
     const { increaseLoading, decreaseLoading } = this.props;
     increaseLoading();
     // const apiKey = process.env.REACT_APP_SPOONACULAR_API_KEY;
-    const apiKey = '502cf6be941843b1899001da2db18cd8';
+    const apiKey = '3b2eff55d72c4e59b6ca95f82dceaacd';
     console.log(process?.env.REACT_APP_SPOONACULAR_API_KEY);
     const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=10&`;
     fetch(url, { method: 'GET' })
